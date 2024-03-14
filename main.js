781c6d04208dfc790ce64d509594edc7051a1c1e
David L. Enock <118814205+davunju@users.noreply.github.com>
2024-03-14 13:11:16 +0300

    updates

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-const button = document.querySelector("#nav-button");
-const button2 = document.querySelector("#nav-button2");
+const button = document.getElementById("nav-button");
+const navLink = document.getElementById("nav-link");
+const arrowDown = document.getElementById("arrow-down");
+const arrowUp = document.getElementById("arrow-up");
 
-const navLink = document.querySelector("#nav-link");
-const navLink2 = document.querySelector("#nav-link2");
-
-const arrowDown = document.querySelector("#arrow-down");
-const arrowUp = document.querySelector("#arrow-up");
-
-const iconUp = document.querySelector("#iconUp");
-const iconDown = document.querySelector("#iconDown");
-
-button.addEventListener("click", (e) => {
-  let toggled;
-  toggled = navLink.classList.toggle("hidden");
+button.addEventListener("click", () => {
+  let toggled = navLink.classList.toggle("hidden");
   if (toggled) {
     arrowDown.classList.remove("hidden");
     arrowUp.classList.add("hidden");
@@ -22,14 +14,34 @@ button.addEventListener("click", (e) => {
   }
 });
 
-button2.addEventListener("click", (e) => {
-  let toggled;
-  toggled = navLink2.classList.toggle("hidden");
+const button2 = document.getElementById("nav-button2");
+const iconDown = document.getElementById("iconDown");
+const iconUp = document.getElementById("iconUp");
+const navLink2 = document.getElementById("nav-link2");
+
+button2.addEventListener("click", () => {
+  let toggled = navLink2.classList.toggle("hidden");
   if (toggled) {
     iconDown.classList.remove("hidden");
     iconUp.classList.add("hidden");
   } else {
     iconDown.classList.add("hidden");
     iconUp.classList.remove("hidden");
+  }
+});
+
+const button3 = document.getElementById("button3");
+const iconMenu = document.getElementById("iconMenu");
+const iconCloseMenu = document.getElementById("iconCloseMenu");
+const sidebar = document.getElementById("sidebar-mobile");
+
+button3.addEventListener("click", () => {
+  let toggled = sidebar.classList.toggle("hidden");
+  if (toggled) {
+    iconCloseMenu.classList.add("hidden");
+    iconMenu.classList.remove("hidden");
+  } else {
+    iconCloseMenu.classList.remove("hidden");
+    iconMenu.classList.add("hidden");
   }
 });
